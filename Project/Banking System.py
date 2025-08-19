b8aa9de3060c3bdc872c74e9e79c320af744e347
RandomAmbitiousCoder <ishankumar.akshaj@gmail.com>
2025-08-19 23:18:25 +0530

    classified login page and menue bar

--- Project/Banking System.py
+++ Project/Banking System.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from PIL import Image
 import mysql.connector as ms
+from CTkMessagebox import CTkMessagebox
 
 
 class App(ctk.CTk):
@@ -37,7 +38,8 @@ class App(ctk.CTk):
 
         self.extend = False
 
-        #Icon Declaration
+
+        #Image Declaration
         self.toggle = ctk.CTkImage(light_image=Image.open("toggle_btn_icon.png"),
                                                          dark_image=Image.open("toggle_btn_icon.png"),
                                                           size = (28,28))
@@ -52,70 +54,114 @@ class App(ctk.CTk):
 
 
         #Frame
-        self.pageframe = ctk.CTkFrame(self)
-        self.pageframe.place(relwidth=1, relheight=1, x = 55)
-        self.menuebar = ctk.CTkFrame(self, fg_color=  '#383838', corner_radius=0)
-        self.menuebar.pack(side = 'left', fill ='both')
-        self.menuebar.propagate(False)
-        self.menuebar.configure(width = 51)
 
 
         #Icons
+        self.pageframe = ctk.CTkFrame(self)
+        
+        self.menuebar = ctk.CTkFrame(self, fg_color=  '#383838', corner_radius=0)
+        
         self.indicator = self.Indicator(self)
-
         self.toggle_menue_button = ctk.CTkButton(self.menuebar, image = self.toggle,fg_color='#383838',hover_color='#303030',width = 0,text = '', command= lambda: self.extend_menue())
         self.toggle_menue_button.place(x =4.5 , y = 10)
-
-        #Screen
-        self.home_screen = self.Home(self)
-
-
-
-
+        
         self.homebtn = ctk.CTkButton(self.menuebar, image=self.home, fg_color='#383838',hover_color='#303030', width=0, text='', height=40,command= lambda: self.indicator.switch_position('home'))
-        self.homebtn.place(x = 4.5, y = 130)
-
         self.homelabel = ctk.CTkLabel(self.menuebar, text='Home', font=('Rockwell', 20 , 'bold'))
-        self.homelabel.place(x= 60, y = 137)
-        self.homelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('home'))
-
-
-
+        
         self.servicebtn = ctk.CTkButton(self.menuebar, image=self.service, fg_color='#383838',hover_color='#303030', width=0, text='',height=40, command= lambda:self.indicator.switch_position('service'))
-        self.servicebtn.place(x = 4.5, y =200 )
-
         self.servicelabel = ctk.CTkLabel(self.menuebar, text='service', font=('Rockwell', 20, 'bold'))
-        self.servicelabel.place(x=60, y=207)
-        self.servicelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('service'))
-
-
-
-
-        self.updatebtn = ctk.CTkButton(self.menuebar, image=self.updated, fg_color='#383838',hover_color='#303030', width=0, text='',height=40, command= lambda: self.indicator.switch_position('update'))
-        self.updatebtn.place(x = 4.5, y = 270)
-
-        self.updatelabel = ctk.CTkLabel(self.menuebar, text='update', font=('Rockwell', 20, 'bold'))
-        self.updatelabel.place(x=60, y=277)
-        self.updatelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('update'))
-
-
+        
         self.contactbtn = ctk.CTkButton(self.menuebar, image=self.contact, fg_color='#383838',hover_color='#303030', width=0, text='',height=40, command= lambda: self.indicator.switch_position('contact'))
-        self.contactbtn.place(x = 4.5, y = 340)
-
         self.contactlabel = ctk.CTkLabel(self.menuebar, text='contact', font=('Rockwell', 20, 'bold'))
-        self.contactlabel.place(x=60, y=347)
-        self.contactlabel.bind('<Button-1>', lambda e: self.indicator.switch_position('contact'))
-
+        
+        self.updatebtn = ctk.CTkButton(self.menuebar, image=self.updated, fg_color='#383838',hover_color='#303030', width=0, text='',height=40, command= lambda: self.indicator.switch_position('update'))
+        self.updatelabel = ctk.CTkLabel(self.menuebar, text='update', font=('Rockwell', 20, 'bold'))
 
         self.about = ctk.CTkButton(self.menuebar, image = self.about, fg_color='#383838',hover_color='#303030', width=0, text='',height=40, command= lambda: self.indicator.switch_position('about'))
-        self.about.place(x = 4.5, y = 410)
-
         self.aboutlabel = ctk.CTkLabel(self.menuebar, text='about', font=('Rockwell', 20, 'bold'))
-        self.aboutlabel.place(x=60, y=417)
-        self.aboutlabel.bind('<Button-1>', lambda e: self.indicator.switch_position('about'))
+
+        #Screen
+        self.login_screen = self.Login(self)
+        
 
 
 
+
+        
+
+
+
+    class Login:
+        def __init__(self,app):
+            self.app = app
+            self.login_frame = ctk.CTkFrame(self.app, fg_color='transparent')
+            self.login_label = ctk.CTkLabel(self.login_frame, text='Login', font=('Rockwell', 50, 'bold'))
+            self.login_username = ctk.CTkEntry(self.login_frame, placeholder_text='Username', width=300, font=('Rockwell', 20))
+            self.login_password = ctk.CTkEntry(self.login_frame, placeholder_text='Password', show='*', width=300, font=('Rockwell', 20))
+            self.login_button = ctk.CTkButton(self.login_frame, text='Login', font=('Rockwell', 20,'bold'),command=lambda:self.login)
+
+            self.login_frame.pack()
+            self.login_label.pack(pady=10)
+            self.login_username.pack(pady=20)
+            self.login_password.pack(pady=(0,20))
+            self.login_button.pack(pady=20)
+        def login(self):
+            self.login_username_value = self.login_username.cget()
+            self.login_password_value = self.login_password.cget()
+            if self.login_username_value and self.login_password_value:
+                self.app.cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (self.login_username_value, self.login_password_value))
+                user = self.app.cursor.fetchone()
+                if user:
+                    self.login_frame.pack_forget()
+                    self.app.pageframe.pack(fill='both', expand=True)
+                    
+                else:
+                    ctk.CTkMessageBox(title="Error", message="Invalid username or password")
+            else:
+                ctk.CTkMessageBox(title="Error", message="Please fill in all fields")
+            
+        
+    
+    class Menue:
+        def __init__(self,app):
+
+            self.app.pageframe.place(relwidth=1, relheight=1, x = 55)
+            self.app.menuebar.pack(side = 'left', fill ='both')
+            self.app.menuebar.propagate(False)
+            self.app.menuebar.configure(width = 51)
+
+            self.app = app
+            self.app.homebtn.place(x = 4.5, y = 130)
+
+            self.app.homelabel.place(x= 60, y = 137)
+            self.app.homelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('home'))
+
+
+
+            self.app.servicebtn.place(x = 4.5, y =200 )
+
+            self.app.servicelabel.place(x=60, y=207)
+            self.app.servicelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('service'))
+
+
+
+
+            self.app.updatebtn.place(x = 4.5, y = 270)
+
+            self.app.updatelabel.place(x=60, y=277)
+            self.app.updatelabel.bind('<Button-1>', lambda e: self.indicator.switch_position('update'))
+
+
+            self.app.contactbtn.place(x = 4.5, y = 340)
+
+            self.app.contactlabel.place(x=60, y=347)
+            self.app.contactlabel.bind('<Button-1>', lambda e: self.indicator.switch_position('contact'))
+
+
+            self.app.about.place(x = 4.5, y = 410)
+
+            self.app.aboutlabel.place(x=60, y=417)
+            self.app.aboutlabel.bind('<Button-1>', lambda e: self.indicator.switch_position('about'))
 
 
 
